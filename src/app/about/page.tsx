1df726c98dbcdc6d64b684d63cc1712523cfdcd6
Ashwin Gattani <ashwin.gattani@accionlabs.com>
2023-09-06 23:17:39 +0530

    added configuration settings for typescript

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <div>This session discusses about different configurations that comes along with NextJS</div>;
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,3 @@
+export default function Contact() {
+    return <div>Feel free to reach out for any questions at ashwin.gattani@accionlabs.com </div>
+}
