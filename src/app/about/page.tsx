68e836a7d0b0524c62ec9553b734b1c4ec7a1084
Ashwin Gattani <ashwin.gattani@accionlabs.com>
2023-09-09 14:08:26 +0530

    added configuration for env variables

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
 export default function About() {
+  console.log(process.env.NEXT_PUBLIC_USERNAME)
   return <div>This session discusses about different configurations that comes along with NextJS</div>;
 }
