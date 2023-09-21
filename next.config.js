0e7f401f3f62087d53472129619671fb8a08fb14
Ashwin Gattani <ashwin.gattani@accionlabs.com>
2023-09-21 15:07:33 +0530

    added config for SWC

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
 };
 
 module.exports = nextConfig;
